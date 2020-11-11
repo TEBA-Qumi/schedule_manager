35f5fc4ad553c1f43ad6ffae5289af6313ec322d
TEBA_Qumi <kumi938150@gmail.com>
2020-11-11 23:45:15 +0900

    Modify validatator

---
+++ src/controllers/teams/TeamsUpdateServlet.java
@@ -0,0 +1,95 @@
+package controllers.teams;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Team;
+import models.validators.TeamValidator;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class TeamsUpdateServlet
+ */
+@WebServlet("/teams/create")
+public class TeamsUpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TeamsUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Team t = em.find(Team.class, (Integer)(request.getSession().getAttribute("team_id")));
+
+            // 現在の値と異なるチーム番号が入力されていたら
+            // 重複チェックを行う指定をする
+            Boolean team_code_duplicate_check = true;
+            if(t.getTeam_code().equals(request.getParameter("team_code"))) {
+                team_code_duplicate_check = false;
+            } else {
+                t.setTeam_code(request.getParameter("team_code"));
+            }
+
+            // パスワード欄に入力があったら
+            // パスワードの入力値チェックを行う指定をする
+            Boolean password_check_flag = true;
+            String password = request.getParameter("password");
+            if(password == null || password.equals("")) {
+                password_check_flag = false;
+            } else {
+                t.setPassword(
+                        EncryptUtil.getPasswordEncrypt(
+                                password,
+                                (String)this.getServletContext().getAttribute("pepper")
+                                )
+                        );
+            }
+
+            t.setName(request.getParameter("name"));
+            t.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+
+            List<String> errors = TeamValidator.validate(t, team_code_duplicate_check, password_check_flag);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("team", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/teams/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("team_id");
+
+                response.sendRedirect(request.getContextPath() + "/");
+            }
+        }
+    }
+
+}
--- src/models/validators/TeamValidator.java
+++ src/models/validators/TeamValidator.java
@@ -3,11 +3,19 @@ package models.validators;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import models.Team;
+import utils.DBUtil;
 
 public class TeamValidator {
-    public static List<String> validate(Team t, Boolean password_check_flag) {
+    public static List<String> validate(Team t, Boolean password_check_flag, Boolean team_code_duplicate_check_flag) {
         List<String> errors = new ArrayList<String>();
+
+        String team_code_error = _validateTeam_code(t.getTeam_code(), team_code_duplicate_check_flag);
+        if(!team_code_error.equals("")) {
+            errors.add(team_code_error);
+        }
 
         String name_error = _validateName(t.getName());
         if(!name_error.equals("")) {
@@ -20,6 +28,27 @@ public class TeamValidator {
         }
 
         return errors;
+    }
+
+    private static String _validateTeam_code(String team_code, Boolean team_code_duplicate_check_flag) {
+        // 必須入力チェック
+        if(team_code == null || team_code.equals("")) {
+            return "チーム番号を入力してください。";
+        }
+
+        // すでに登録されている社員番号との重複チェック
+        if(team_code_duplicate_check_flag) {
+            EntityManager em = DBUtil.createEntityManager();
+            long teams_count = (long)em.createNamedQuery("checkRegisteredTeam_code", Long.class)
+                                           .setParameter("team_code", team_code)
+                                             .getSingleResult();
+            em.close();
+            if(teams_count > 0) {
+                return "入力されたチーム番号はすでに存在しています。";
+            }
+        }
+
+        return "";
     }
 
     private static String _validateName(String name) {
